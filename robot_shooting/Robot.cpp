4d828f55adf36a53cdb2bd02875a8fcf3d9f98ce
Tak Oshima <simtak89@gmail.com>
2020-01-13 20:05:51 -0700

    Update Robot.cpp

--- robot_shooting/Robot.cpp
+++ robot_shooting/Robot.cpp
@@ -4,10 +4,6 @@
 //  Robot implementation
 ///////////////////////////////////////////////////////////////////////////
 
-// A robot is created at some position (using the Arena coordinate system, where row 1, column 1 is the upper-left-most position, etc.).
-// You may tell a Robot to move.
-// You may ask a Robot object for its position.
-
 Robot::Robot(Arena* ap, int r, int c) {
     if (ap == nullptr) {
         cout << "***** A robot must be in some Arena!" << endl;
@@ -36,7 +32,7 @@ void Robot::move() {
     // Attempt to move in a random direction; if we can't move, don't move
     switch (TESTrand() % 4) {
         case UP:
-            // TODO:  Move the robot up one row if possible.
+            // Move the robot up one row if possible.
             if (m_row > 1)
                 m_row--;
             break;
@@ -58,9 +54,6 @@ void Robot::move() {
 }
 
 bool Robot::takeDamageAndLive() {
-    // TODO:  If the robot has been hit once before, return false (since a
-    // second hit kills a robot).  Otherwise, return true (since the robot
-    // survived the damage).
     if (damaged)
         return false;
     else {
@@ -69,14 +62,11 @@ bool Robot::takeDamageAndLive() {
     }
 }
 
-/*
 void Robot::takeDamage() {
     if (damaged == false)
         damaged = true;
     else {
         cout << "Error: Robot was damaged for second time." << endl;
         exit(1);
-    }
-        
+    }    
 }
- */
