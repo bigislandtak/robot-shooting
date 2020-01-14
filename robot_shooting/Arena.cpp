8cb2a3adb65750b39658fa2e78957bf240edbba6
Tak Oshima <simtak89@gmail.com>
2020-01-13 19:58:40 -0700

    Update Arena.cpp

--- robot_shooting/Arena.cpp
+++ robot_shooting/Arena.cpp
@@ -1,17 +1,10 @@
 #include "Arena.h"
 #include "Player.h"
 #include "Robot.h"
+
 ///////////////////////////////////////////////////////////////////////////
 //  Arena implementations
 ///////////////////////////////////////////////////////////////////////////
-
-// When an Arena object of a particular size is created, it has no robots or player. In the Arena coordinate system, row 1, column 1 is the upper-left-most position that can be occupied by a Robot or Player. (If an Arena were created with 10 rows and 20 columns, then the lower-right-most position that could be occupied would be row 10, column 20.)
-// You may tell the Arena object to create or destroy a Robot at a particular position.
-// You may tell the Arena object to create a Player at a particular position.
-// You may tell the Arena object to have all the robots in it make their move.
-// You may ask the Arena object its size, how many robots are at a particular position, and how many robots altogether are in the Arena.
-// You may ask the Arena object for access to its player.
-// An Arena object may be displayed on the screen, showing the locations of the robots and player, along with other status information.
 
 Arena::Arena(int nRows, int nCols) {
     if (nRows <= 0  ||  nCols <= 0  ||  nRows > MAXROWS  ||  nCols > MAXCOLS) {
@@ -42,14 +35,10 @@ Arena::~Arena() {
 }
 
 int Arena::rows() const {
-    // TODO:  TRIVIAL:  Return the number of rows in the arena.
-    // Delete the following line and replace it with the correct code.
     return m_rows;
 }
 
 int Arena::cols() const {
-    // TODO:  TRIVIAL:  Return the number of columns in the arena.
-    // Delete the following line and replace it with the correct code.
     return m_cols;
 }
 
@@ -62,7 +51,6 @@ int Arena::robotCount() const {
 }
 
 int Arena::nRobotsAt(int r, int c) const {
-    // TODO:  Return the number of robots at row r, column c.
     int count = 0;
     for (int i = 0, j = 0; i < MAXROBOTS && j < m_nRobots; i++) {   // i iterates through the array, j keeps track of the number of robots seen so far.
         if (m_robots[i] == nullptr)
@@ -87,9 +75,9 @@ void Arena::display(string msg) const {
             grid[r][c] = '.';
     
     // Indicate each robot's position
-    // TODO:  If one robot is at some grid point, set the char to 'R'.
-    //        If it's 2 though 8, set it to '2' through '8'.
-    //        For 9 or more, set it to '9'.
+    // If one robot is at some grid point, set the char to 'R'.
+    // If it's 2 though 8, set it to '2' through '8'.
+    // For 9 or more, set it to '9'.
     for (r = 0; r < rows(); r++) {
         for (c = 0; c < cols(); c++) {
             int nRobots = nRobotsAt(r+1, c+1);
@@ -141,11 +129,10 @@ bool Arena::addRobot(int r, int c) {
     // If MAXROBOTS have already been added, return false.  Otherwise,
     // dynamically allocate a new robot at coordinates (r,c).  Save the
     // pointer to the newly allocated robot and return true.
-    
-    // TODO:  Implement this.
+
     if (m_nRobots >= MAXROBOTS)
         return false;
-    for (int i = 0; i < MAXROBOTS; i++) {   // Create new Robot at r, c and assign pointer to first nullptr in m_robots.
+    for (int i = 0; i < MAXROBOTS; i++) {       // Create new Robot at r, c and assign pointer to first nullptr in m_robots.
         if (m_robots[i] == nullptr) {
             m_robots[i] = new Robot(this, r, c);
             m_nRobots++;
@@ -163,33 +150,15 @@ bool Arena::addPlayer(int r, int c) {
     
     // Dynamically allocate a new Player and add it to the arena
     m_player = new Player(this, r, c);
-    //cout << "Player added" << endl;
     return true;
 }
 
 void Arena::damageRobotAt(int r, int c) {
-    // TODO:  Damage one robot at row r, column c if at least one is there.
     // If the robot does not survive the damage, destroy it.
-    /*
-    const int targets = nRobotsAt(r, c);
-    if (targets == 0)
-        return;
-    Robot* targetRobots[targets];
-    for (int i = 0, j = 0; i < m_nRobots && j < targets; i++) {
-        if (m_robots[i]->row() == r && m_robots[i]->col() == c)
-            targetRobots[j++] = m_robots[i];
-    }
-    int unluckyRobot = rand() % targets;
-    if(targetRobots[unluckyRobot]->takeDamageAndLive())
-        targetRobots[unluckyRobot]->takeDamage();
-    else
-        delete targetRobots[unluckyRobot];
-     */
     if (nRobotsAt(r, c) == 0)
         return;
     
     for (int i = 0, j = 0; i < MAXROBOTS && j < m_nRobots; i++) {
-        //cout << "damageRobotAt() loop " << i+1 << endl;
         if (m_robots[i] == nullptr)
             continue;
         else
@@ -197,7 +166,7 @@ void Arena::damageRobotAt(int r, int c) {
         
         if (m_robots[i]->row() == r && m_robots[i]->col() == c) {
             if (m_robots[i]->takeDamageAndLive()) {
-                //m_robots[i]->takeDamage();
+                m_robots[i]->takeDamage();
                 return;
             }
             else {
@@ -214,9 +183,8 @@ void Arena::damageRobotAt(int r, int c) {
 
 bool Arena::moveRobots() {
     for (int i= 0, j = 0; i < MAXROBOTS && j < m_nRobots; i++) {
-        // TODO:  Have the k-th robot in the arena make one move.
-        //        If that move results in that robot being in the same
-        //        position as the player, the player dies.
+        //  If that move results in that robot being in the same
+        //  position as the player, the player dies.
         if (m_robots[i] == nullptr)
             continue;
         else
