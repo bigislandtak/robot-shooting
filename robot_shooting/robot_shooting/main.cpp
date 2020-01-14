ea05be8aefea6bdc2731a333c0ce2414e844e80a
Tak Oshima <simtak89@gmail.com>
2020-01-13 17:55:52 -0700

    Add files via upload

---
+++ robot_shooting/robot_shooting/main.cpp
@@ -0,0 +1,18 @@
+#include "Game.h"
+#include "Robot.h"
+#include "Player.h"
+#include "globals.h"
+#include "Arena.h"
+
+int main() {
+    // Initialize the random number generator.  (You don't need to
+    // understand how this works.)
+    srand(static_cast<unsigned int>(time(0)));
+    
+    // Create a game
+    // Use this instead to create a mini-game:   Game g(3, 3, 2);
+    Game g(15, 18, 80);
+    
+    // Play the game
+    g.play();
+}
