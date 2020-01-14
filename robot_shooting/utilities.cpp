796484f9e13e4f5c1c9c78e4fae59006e799e4a8
Tak Oshima <simtak89@gmail.com>
2020-01-13 20:18:02 -0700

    Update utilities.cpp

--- robot_shooting/utilities.cpp
+++ robot_shooting/utilities.cpp
@@ -22,8 +22,7 @@ int decodeDirection(char dir) {
 //  clearScreen implementations
 ///////////////////////////////////////////////////////////////////////////
 
-// DO NOT MODIFY OR REMOVE ANY CODE BETWEEN HERE AND THE END OF THE FILE!!!
-// THE CODE IS SUITABLE FOR VISUAL C++, XCODE, AND g++ UNDER LINUX.
+// THIS CODE IS SUITABLE FOR VISUAL C++, XCODE, AND g++ UNDER LINUX.
 
 // Note to Xcode users:  clearScreen() will just write a newline instead
 // of clearing the window if you launch your program from within Xcode.
@@ -35,16 +34,15 @@ int decodeDirection(char dir) {
 
 void clearScreen() {
     /*
-     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     CONSOLE_SCREEN_BUFFER_INFO csbi;
     GetConsoleScreenBufferInfo(hConsole, &csbi);
     DWORD dwConSize = csbi.dwSize.X * csbi.dwSize.Y;
     COORD upperLeft = { 0, 0 };
     DWORD dwCharsWritten;
-    FillConsoleOutputCharacter(hConsole, TCHAR(' '), dwConSize, upperLeft,
-                               &dwCharsWritten);
+    FillConsoleOutputCharacter(hConsole, TCHAR(' '), dwConSize, upperLeft, &dwCharsWritten);
     SetConsoleCursorPosition(hConsole, upperLeft);
-     */
+    */
 }
 
 #else  // not Microsoft Visual C++, so assume UNIX interface
