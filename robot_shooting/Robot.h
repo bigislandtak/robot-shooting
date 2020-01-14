3684dcfb8a0c5f1487b40a6cbb45310e185d4a54
Tak Oshima <simtak89@gmail.com>
2020-01-13 20:06:29 -0700

    Update Robot.h

--- robot_shooting/Robot.h
+++ robot_shooting/Robot.h
@@ -6,8 +6,7 @@
 #include "Arena.h"
 
 class Arena;  // This is needed to let the compiler know that Arena is a
-// type name, since it's mentioned in the Robot declaration.
-
+              // type name, since it's mentioned in the Robot declaration.
 class Robot {
 public:
     // Constructor
@@ -28,9 +27,6 @@ private:
     Arena* m_arena;
     int    m_row;
     int    m_col;
-    // TODO: You'll probably find that a robot object needs an additional
-    // data member to support your implementation of the behavior affected
-    // by being hit and taking damage.
     bool damaged;   // Keeps track if robot has been hit before
 };
 
